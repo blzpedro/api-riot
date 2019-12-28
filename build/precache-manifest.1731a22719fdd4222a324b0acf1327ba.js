db9872b232c1ad314153b7db14c897c61a3f1384
blzpedro <phenriqmelo99@gmail.com>
2019-12-28 19:31:40 -0300

    deploy heroku

---
+++ build/precache-manifest.1731a22719fdd4222a324b0acf1327ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f29fb0f93f72815279b5307fec14707",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60bd3473f26b79bdb951",
+    "url": "/static/css/main.2434318b.chunk.css"
+  },
+  {
+    "revision": "416ba913004213fe9b6d",
+    "url": "/static/js/2.24d11822.chunk.js"
+  },
+  {
+    "revision": "5a0d989568a936270ef6c2e0e2d0229c",
+    "url": "/static/js/2.24d11822.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60bd3473f26b79bdb951",
+    "url": "/static/js/main.b598122b.chunk.js"
+  },
+  {
+    "revision": "6de2a4bf761b1bf1b711",
+    "url": "/static/js/runtime-main.40b2820a.js"
+  }
+]);
